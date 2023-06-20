d74f08817c66ab89b6b7411703e358f789badd88
SandhyaBharath01 <sandhya.pujari26@gmail.com>
2023-06-20 14:37:00 +0530

    going live

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("groupchat", "root", "sandhya", {
-    dialect : "mysql",
-    host : "localhost",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    dialect: "mysql",
+    host:process.env.DB_HOST
 });
 
 module.exports = sequelize;
